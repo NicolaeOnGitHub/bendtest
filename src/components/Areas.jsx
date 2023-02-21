02ae35a22a94fe52e1ec8a9ceac8e4eb4b47d55b
Nicolae <olari.nicolae@mail.ru>
2023-02-21 23:20:09 +0300

    first commit

---
+++ src/components/Areas.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Area from "./Area";
+
+export default function Areas() {
+    const [areas, setAreas] = useState([]);
+
+    const getAreas = async () => {
+        const res = await fetch("areas.json");
+        const data = await res.json();
+        setAreas(data);
+    };
+
+    useEffect(() => {
+        getAreas();
+    }, []);
+
+    return (
+        <>
+            {areas.map((area) => {
+                return <Area key={area.areaId} area={area} />;
+            })}
+        </>
+    );
+}
+{}
